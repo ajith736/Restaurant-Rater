44ad52bb8e4ddeae3f96d615d44c85e371c856b4
ajith736 <ajithc736@gmail.com>
2024-07-26 19:48:55 +0530

    first commit

---
+++ restaurant/server/db/db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name TEXT,
+    location TEXT,
+    price_range TEXT
+);
+
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL check(
+        rating >= 1
+        and rating <= 5
+    )
+);
